299ab694b0ff05d2bf7a214d2186f245feca1d46
Luis Alfredo Cardona Bazán <Alfredo@iMac.local>
2024-05-08 17:17:25 -0600

    Mis prácticas de ´ython

---
+++ print.py
@@ -0,0 +1,21 @@
+# Ejemplos de la función print ()
+
+# print('Hola mundo')
+# print('Hola mundo' , 'otra vez')
+# print('son las', 9, 'de la mañana')
+# print ('El resultado de 3 * 4 es:', 3*4)
+
+# Ejemplos de cadenas formateadas
+# print('El número 15 en sistema deciaml el %d, en sistema octal es %o, en el sistema hexadeciaml es %x' % (15, 15, 15))
+
+# pi = 3.141592
+# r = 5
+# print(f'El radio de un círuclo es {r} y el área de ese círculo es {pi * r ** 2 : .2f}')
+
+# Impresión de caractéres especiales
+print('La letra beta es \n\t \u03B2')
+
+# Caracteres de escape
+print('Hola mundo', end = ' ')
+print('otra vez', end = '\t')
+print('y otra vez')
